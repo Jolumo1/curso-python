c20cdaf134a6684a0dd054e784cedb99f1822f35
Jolumo1 <jo_lumo@outlook.com>
2025-01-22 20:20:01 +0100

    Add solutions for basic exercises in Python, including printing variables, type casting, and using f-strings - Lesson 1

--- 01_basic/exercises.py
+++ 01_basic/exercises.py
@@ -6,7 +6,8 @@
 print("\nEjercicio 1: Imprimir mensajes")
 print("Escribe un programa que imprima tu nombre y tu ciudad en líneas separadas.")
 
-### Completa aquí
+print("Jose")
+print("Málaga");
 
 print("--------------")
 
@@ -18,7 +19,11 @@ c = "Hola mundo"
 d = True
 e = None
 
-### Completa aquí
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
 
 print("--------------")
 
@@ -26,7 +31,8 @@ print("\nEjercicio 3: Casting de tipos")
 print("Convierte la cadena \"12345\" a un entero y luego a un float.")
 print("Convierte el float 3.99 a un entero. ¿Qué ocurre?")
 
-### Completa aquí
+print(type(int("12345")))
+print(int(3.99)) #Se pierde la parte decimal
 
 print("--------------")
 
@@ -36,7 +42,11 @@ print("Usa f-strings para imprimir una presentación.")
 
 # "Hola! Me llamo midudev y tengo 39 años, mido 1.70 metros"
 
-### Completa aquí
+nombre = "Jose"
+edad = 37
+altura = 1.78
+
+print(f"Hola! Me llamo {nombre} y tengo {edad} años, mido {altura} metros")
 
 print("--------------")
 
@@ -45,3 +55,6 @@ print("1. Crea una variable con el número PI (sin asignar una variable)")
 print("2. Redondea el número con round()")
 print("3. Haz la división entera entre el número que te salió y el número 2")
 print("4. El resultado debería ser 1")
+
+
+print(round(3.14159)//2) # // para división entera. El resultado es 1.
