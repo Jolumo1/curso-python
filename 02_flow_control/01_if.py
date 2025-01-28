9a06ac813fc77114e8d46ce7b477c4d50e29f232
Jolumo1 <jo_lumo@outlook.com>
2025-01-28 10:53:44 +0100

    Exercises completed

--- 02_flow_control/01_if.py
+++ 02_flow_control/01_if.py
@@ -4,38 +4,39 @@
 ###
 
 import os
+
 os.system("clear")
 
 print("\n Sentencia simple condicional")
 
 edad = 18
 if edad >= 18:
-  print("Eres mayor de edad")
-  print("¡Felicidades!")
+    print("Eres mayor de edad")
+    print("¡Felicidades!")
 
 edad = 15
 if edad >= 18:
-  print("Eres mayor de edad")
-  print("¡Felicidades!")
+    print("Eres mayor de edad")
+    print("¡Felicidades!")
 
 print("\n Sentencia condicional con else")
 edad = 15
 if edad >= 18:
-  print("Eres mayor de edad")
+    print("Eres mayor de edad")
 else:
-  print("Eres menor de edad")
+    print("Eres menor de edad")
 
 print("\n Sentencia condicional con elif")
 nota = 5
 
 if nota >= 9:
-  print("¡Sobresaliente!")
+    print("¡Sobresaliente!")
 elif nota >= 7:
-  print("Notable!")
+    print("Notable!")
 elif nota >= 5:
-  print("¡Aprobado!")
+    print("¡Aprobado!")
 else:
-  print("¡No está calificado!")
+    print("¡No está calificado!")
 
 print("\n Condiciones múltiples")
 edad = 16
@@ -47,20 +48,20 @@ tiene_carnet = True
 
 # 🇻🇪 un pueblo de Valencia
 if edad >= 18 and tiene_carnet:
-  print("Puedes conducir 🚗")
+    print("Puedes conducir 🚗")
 else:
-  print("POLICIA 🚔!!!1!!!")
+    print("POLICIA 🚔!!!1!!!")
 
 # 🇻🇪 un pueblo de Isla Margarita
 if edad >= 18 or tiene_carnet:
-  print("Puedes conducir en la Isla Margarita 🚗")
+    print("Puedes conducir en la Isla Margarita 🚗")
 else:
-  print("Paga al policía y te deja conducir!!!")
+    print("Paga al policía y te deja conducir!!!")
 
 es_fin_de_semana = False
 # JavaScript -> !
 if not es_fin_de_semana:
-  print("¡midu, venga que hay que streamear!")
+    print("¡midu, venga que hay que streamear!")
 
 
 print("\n Anidar condicionales")
@@ -68,12 +69,12 @@ edad = 20
 tiene_dinero = True
 
 if edad >= 18:
-  if tiene_dinero:
-    print("Puedes ir a la discoteca")
-  else:
-    print("Quédate en casa")
+    if tiene_dinero:
+        print("Puedes ir a la discoteca")
+    else:
+        print("Quédate en casa")
 else:
-  print("No puedes entrar a la disco")
+    print("No puedes entrar a la disco")
 
 # Más fácil:
 # if edad < 18:
@@ -84,22 +85,22 @@ else:
 #   print("Quédate en casa")
 
 numero = 5
-if numero: # True
-  print("El número no es cero")
+if numero:  # True
+    print("El número no es cero")
 
 numero = 0
-if numero: # False
-  print("Aquí no entrará nunca")
+if numero:  # False
+    print("Aquí no entrará nunca")
 
 nombre = ""
 if nombre:
-  print("El nombre no es vacío")
+    print("El nombre no es vacío")
 
-numero = 3 # asignación
-es_el_tres = numero == 3 # comparación
+numero = 3  # asignación
+es_el_tres = numero == 3  # comparación
 
 if es_el_tres:
-  print("El número es 3")
+    print("El número es 3")
 
 
 print("\nLa condición ternaria:")
@@ -118,13 +119,50 @@ print(mensaje)
 # Pide al usuario que introduzca dos números y muestra un mensaje
 # indicando cuál es mayor o si son iguales
 
+num1 = input("Introduce el primer número: ")
+num2 = input("Introduce el segundo número: ")
+
+if num1 > num2:
+    print(f"El primero número: {num1}, es el mayor")
+elif num1 < num2:
+    print(f"El segundo número: {num2}, es el mayor")
+else:
+    print("Los dos números son iguales")
+
+
 # Ejercicio 2: Calculadora simple
 # Pide al usuario dos números y una operación (+, -, *, /)
 # Realiza la operación y muestra el resultado (maneja la división entre zero)
 
+num1 = float(input("Introduce el primer número: "))
+num2 = float(input("Introduce el segundo número: "))
+operacion = input("Introduce la operación (+, -, *, /): ")
+if operacion == "+":
+    resultado = num1 + num2
+elif operacion == "-":
+    resultado = num1 - num2
+elif operacion == "*":
+    resultado = num1 * num2
+elif operacion == "/":
+    if num2 == 0:
+        resultado = "No se puede dividir entre cero"
+    else:
+        resultado = num1 / num2
+
+print(f"El resultado de la operación es: {resultado}")
+
+
 # Ejercicio 3: Año bisiesto
 # Pide al usuario que introduzca un año y determina si es bisiesto.
 # Un año es bisiesto si es divisible por 4, excepto si es divisible por 100 pero no por 400.
+
+year = int(input("Introduce un año para ver si es bisiesto"))
+
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+    print(f"El año {year} es bisiesto")
+else:
+    print(f"El año {year} no es bisiesto")
+
 
 # Ejercicio 4: Categorizar edades
 # Pide al usuario que introduzca una edad y la clasifique en:
@@ -133,3 +171,19 @@ print(mensaje)
 # - Adolescente (13-17 años)
 # - Adulto (18-64 años)
 # - Adulto mayor (65 años o más)
+
+
+edad = int(input("Introduce una edad para categorizarla: "))
+
+if edad < 0:
+    print("Número erróneo")
+elif edad > 64:
+    print("Adulto mayor")
+elif edad > 17:
+    print("Adulto")
+elif edad > 12:
+    print("Adolescente")
+elif edad > 2:
+    print("Niño")
+else:
+    print("Bebé")
--- 02_flow_control/04_list_methods.py
+++ 02_flow_control/04_list_methods.py
@@ -4,71 +4,74 @@
 ###
 
 import os
+
 os.system("clear")
 
-lista1 = ['a', 'b', 'c', 'd']
+lista1 = ["a", "b", "c", "d"]
 
 # Añadir o insertar elementos a la lista
 
-lista1.append('e') # Añade un elemento al final
+lista1.append("e")  # Añade un elemento al final
 print(lista1)
 
-lista1.insert(1, '@') # Inserta un elemento en la posición que le indiquemos como primer argumento
+lista1.insert(
+    1, "@"
+)  # Inserta un elemento en la posición que le indiquemos como primer argumento
 print(lista1)
 
-lista1.extend(['😃', '😍']) # Agrega elementos al final de la lista
+lista1.extend(["😃", "😍"])  # Agrega elementos al final de la lista
 print(lista1)
 
 # Eliminar elementos de la lista
-lista1.remove('@') # Eliminar la primera aparición de la cadena de texto @
+lista1.remove("@")  # Eliminar la primera aparición de la cadena de texto @
 print(lista1)
 
-ultimo = lista1.pop() # Eliminar el último elemento de la lista y además te lo devuelve
+ultimo = lista1.pop()  # Eliminar el último elemento de la lista y además te lo devuelve
 print(ultimo)
 print(lista1)
 
-lista1.pop(1) # Eliminar el segundo elemento de la lista (es el índice 1)
+lista1.pop(1)  # Eliminar el segundo elemento de la lista (es el índice 1)
 print(lista1)
 
 # Eliminar por lo bestia
 del lista1[-1]
 print(lista1)
 
-lista1.clear() # Eliminar todos los elementos de la lista
+lista1.clear()  # Eliminar todos los elementos de la lista
 print(lista1)
 
 # Eliminar un rango de elementos
-lista1 = ['🐼', '🐨', '🐶', '😿', '🐹']
+lista1 = ["🐼", "🐨", "🐶", "😿", "🐹"]
 del lista1[1:3]
 print(lista1)
 
 # Más métodos útiles
-print('Ordenar listas modificando la original')
+print("Ordenar listas modificando la original")
 numbers = [3, 10, 2, 8, 99, 101]
 numbers.sort()
 print(numbers)
 
-print('Ordenar listas creando una nueva lista')
+print("Ordenar listas creando una nueva lista")
 numbers = [3, 10, 2, 8, 99, 101]
 sorted_numbers = sorted(numbers)
 print(sorted_numbers)
 
 print("Ordenar una lista de cadenas de texto (todo minúscula)")
-frutas = ['manzana', 'pera', 'limón', 'manzana', 'pera', 'limón']
+frutas = ["manzana", "pera", "limón", "manzana", "pera", "limón"]
 sorted_frutas = sorted(frutas)
 print(sorted_frutas)
 
 print("Ordenar una lista de cadenas de texto (mezclas mayúscula y minúscula)")
-frutas = ['manzana', 'Pera', 'Limón', 'manzana', 'pera', 'limón']
+frutas = ["manzana", "Pera", "Limón", "manzana", "pera", "limón"]
 frutas.sort(key=str.lower)
 print(frutas)
 
-# Más cositas útiles
-animals = ['🐶', '🐼', '🐨', '🐶']
-print(len(animals)) # Tamaño de la listas -> 4
-print(animals.count('🐶')) # Cuantas veces aparece el elemento '🐶' -> 2
-print('🐼' in animals) # Comprueba si hay un '🐼' en la lista -> True
-print('🐹' in animals) # -> False
+# Más cositas útiles
+animals = ["🐶", "🐼", "🐨", "🐶"]
+print(len(animals))  # Tamaño de la listas -> 4
+print(animals.count("🐶"))  # Cuantas veces aparece el elemento '🐶' -> 2
+print("🐼" in animals)  # Comprueba si hay un '🐼' en la lista -> True
+print("🐹" in animals)  # -> False
 
 ###
 # EJERCICOS
@@ -81,6 +84,14 @@ print('🐹' in animals) # -> False
 # Inserta el número 10 en la posición 2 usando insert().
 # Modifica el primer elemento de la lista para que sea 0.
 
+lista = [1, 2, 3, 4, 5]
+lista.append(6)
+lista.insert(2, 10)
+lista[0] = 0
+
+print(lista)
+
+
 # Ejercicio 2: Combinar y limpiar listas
 # Crea dos listas:
 # lista_a = [1, 2, 3]
@@ -90,16 +101,43 @@ print('🐹' in animals) # -> False
 # Elimina el elemento en el índice 3 de lista_a usando pop(). Imprime el elemento eliminado.
 # Limpia completamente lista_b usando clear().
 
+lista_a = [1, 2, 3]
+lista_b = [4, 5, 6, 1, 2]
+
+lista_a.extend(lista_b)
+lista_a.remove(1)
+
+print(f"Eliminado: {lista_a.pop(3)}")
+lista_b.clear()
+
+print("Lista a:", lista_a)
+print("Lista b:", lista_b)
+
+
 # Ejercicio 3: Slicing y eliminación con del
 # Crea una lista con los números del 1 al 10.
 # Utiliza slicing y del para eliminar los elementos desde el índice 2 hasta el 5 (sin incluir el 5).
 # Imprime la lista resultante.
+
+numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+del numeros[2:5]
+print(numeros)
+
 
 # Ejercicio 4: Ordenar y contar
 # Crea una lista con los siguientes números: [5, 2, 8, 1, 9, 4, 2].
 # Ordena la lista de forma ascendente usando sort().
 # Cuenta cuántas veces aparece el número 2 en la lista usando count().
 # Comprueba si el número 7 está en la lista usando in.
+
+
+lista_4 = [5, 2, 8, 1, 9, 4, 2]
+lista_4.sort()
+print(lista_4)
+
+print(f"El numero 2 aparece {lista_4.count(2)} veces")
+print(f"El numero 7 está en la lista? {7 in lista_4}")
+
 
 # Ejercicio 5: Copia vs. Referencia
 # Crea una lista llamada original con los números [1, 2, 3].
@@ -109,6 +147,26 @@ print('🐹' in animals) # -> False
 # Modifica el primer elemento de la lista referencia a 10.
 # Imprime las cuatro listas (original, copia_1, copia_2, referencia) y observa los cambios.
 
+original = [1,2,3]
+copia_1 = original[:]
+copia_2 = copia_1.copy()
+
+referencia = original
+
+referencia[0] = 10
+
+print(original)
+print(copia_1)
+print(copia_2)
+print(referencia)
+
+
 # Ejercicio 6: Ordenar strings sin diferenciar mayúsculas y minúsculas.
 # Crea una lista con las siguientes cadenas: ["Manzana", "pera", "BANANA", "naranja"].
 # Ordena la lista sin diferenciar entre mayúsculas y minúsculas.
+
+frutas = ["Manzana", "pera", "BANANA", "naranja"]
+frutas.sort(key=str.lower)
+
+print(frutas)
+
